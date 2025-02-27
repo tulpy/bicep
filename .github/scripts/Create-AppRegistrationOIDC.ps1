98686ceb86b05f9ba372e783c4a28ba71d93baee
tulpy <stephen.tulp@insight.com>
2025-02-27 16:45:25 +0800

    Refactor Create-AppRegistrationOIDC script: comment out unnecessary module installations, reduce sleep duration, and improve code readability

--- .github/scripts/Create-AppRegistrationOIDC.ps1
+++ .github/scripts/Create-AppRegistrationOIDC.ps1
@@ -13,9 +13,9 @@ param(
   [string]$gitHubRepoName
 )
 
-Install-Module Microsoft.Graph -Force -verbose
-Get-Module Microsoft.Graph
-Import-Module Microsoft.Graph -Force
+# Install-Module Microsoft.Graph -Force -verbose
+# Get-Module Microsoft.Graph
+# Import-Module Microsoft.Graph -Force
 
 $AccessToken = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com" -AsSecureString).Token
 (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token
@@ -27,27 +27,25 @@ Import-Module Microsoft.Graph.Applications
 $myApp = Get-MgApplication -Filter "DisplayName eq '$applicationRegistrationName'" -ErrorAction SilentlyContinue        
 if ($myApp) {
   Write-Output "Application Registration $applicationRegistrationName already exists. Skipping creation..."
-}
-else {
+} else {
   $myApp = New-MgApplication -DisplayName "$applicationRegistrationName"
   Write-Output "Application Registration $applicationRegistrationName created successfully."
 }
-$myApp
 
-start-sleep -Seconds 300
+Start-Sleep -Seconds 100
 $subscriptionId = (Get-AzContext).Subscription.Id
 $tenantId = (Get-AzContext).Subscription.TenantId
 $gitBranch = "main"
 $gitFederationName = "fed-credentials-$($workloadShortName)"
 $clientId = $myApp.id
-$policy = "repo:$gitHubOrganisation/$($githubRepoName):ref:refs/heads/$gitBranch"
-$creds = get-MgApplicationFederatedIdentityCredential -ApplicationId $myApp.id | where-object { $_.Subject -eq $policy }
+$policy = "repo:$gitHubOrganisation/$($gitHubRepoName):ref:refs/heads/$gitBranch"
+$creds = Get-MgApplicationFederatedIdentityCredential -ApplicationId $myApp.id | Where-Object { $_.Subject -eq $policy }
 
 if ($creds) {
   Write-Output "Policy details $policy already exists. Skipping creation..."
-}
-else {
+} else {
   $federatedApp = New-MgApplicationFederatedIdentityCredential -ApplicationId $clientId `
     -Audiences api://AzureADTokenExchange -Issuer "https://token.actions.githubusercontent.com" -Name $gitFederationName -Subject $policy
 }
+
 Write-Output "devObjectId=$myApp.ObjectId" >> $env:GITHUB_ENV
