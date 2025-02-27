967660b52ffdc2bb15a58664ab506517b7032564
tulpy <stephen.tulp@insight.com>
2025-02-27 16:28:24 +0800

    Refactor OIDC application registration script and update workflows to use new parameter names for improved clarity and consistency

--- .github/scripts/Create-AppRegistrationOIDC.ps1
+++ .github/scripts/Create-AppRegistrationOIDC.ps1
@@ -1,34 +1,53 @@
-            # Install-Module Microsoft.Graph -Force -verbose
-            # Get-Module Microsoft.Graph
-            # Import-Module Microsoft.Graph -Force
-            $AccessToken = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com" -AsSecureString).Token
-            (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token
-            $sec = ConvertTo-SecureString $AccessToken -AsPlainText -Force
-            Connect-MgGraph -AccessToken $sec -NoWelcome
-            Import-Module Microsoft.Graph.Applications
-            $appName = "${{ env.spnDevName }}"
-            $myApp = Get-MgApplication -Filter "DisplayName eq '$appName'" -ErrorAction SilentlyContinue        
-            if ($myApp) {
-                Write-Output "Application Registration $appName already exists. Skipping creation..."
-            } else {
-                $myApp = New-MgApplication -DisplayName "$appName"
-                Write-Output "Application Registration $appName created successfully."
-            }
-            $myApp 
-            start-sleep -Seconds 300
-            $subscriptionId = (Get-AzContext).Subscription.Id
-            $tenantId = (Get-AzContext).Subscription.TenantId
-            $githubOrganisation = "tulpy"
-            $githubRepo = "${{ inputs.gitHubRepoName}}"
-            $gitBranch = "main"
-            $gitFederationName = "fed-credentials-${{ env.workloadShortName }}"
-            $clientId = $myApp.id
-            $policy = "repo:$githubOrganisation/$($githubRepo):ref:refs/heads/$gitBranch"
-            $creds = get-MgApplicationFederatedIdentityCredential -ApplicationId $myApp.id | where-object {$_.Subject -eq $policy}
-            if($creds){
-              Write-Output "Policy details $policy already exists. Skipping creation..."
-            } else {
-              $federatedApp = New-MgApplicationFederatedIdentityCredential -ApplicationId $clientId `
-              -Audiences api://AzureADTokenExchange -Issuer "https://token.actions.githubusercontent.com" -Name $gitFederationName -Subject $policy
-            }
-            echo "devObjectId=$myApp.ObjectId" >> $env:GITHUB_ENV
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$workloadShortName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$applicationRegistrationName,
+
+  [Parameter(Mandatory = $true)]
+  [string]$gitHubOrganisation,
+
+  [Parameter(Mandatory = $true)]
+  [string]$gitHubRepoName
+)
+
+Install-Module Microsoft.Graph -Force -verbose
+Get-Module Microsoft.Graph
+Import-Module Microsoft.Graph -Force
+
+$AccessToken = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com" -AsSecureString).Token
+(Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com").Token
+$sec = ConvertTo-SecureString $AccessToken -AsPlainText -Force
+
+Connect-MgGraph -AccessToken $sec -NoWelcome
+Import-Module Microsoft.Graph.Applications
+
+$myApp = Get-MgApplication -Filter "DisplayName eq '$applicationRegistrationName'" -ErrorAction SilentlyContinue        
+if ($myApp) {
+  Write-Output "Application Registration $applicationRegistrationName already exists. Skipping creation..."
+}
+else {
+  $myApp = New-MgApplication -DisplayName "$applicationRegistrationName"
+  Write-Output "Application Registration $applicationRegistrationName created successfully."
+}
+$myApp
+
+start-sleep -Seconds 300
+$subscriptionId = (Get-AzContext).Subscription.Id
+$tenantId = (Get-AzContext).Subscription.TenantId
+$gitBranch = "main"
+$gitFederationName = "fed-credentials-$($workloadShortName)"
+$clientId = $myApp.id
+$policy = "repo:$gitHubOrganisation/$($githubRepoName):ref:refs/heads/$gitBranch"
+$creds = get-MgApplicationFederatedIdentityCredential -ApplicationId $myApp.id | where-object { $_.Subject -eq $policy }
+
+if ($creds) {
+  Write-Output "Policy details $policy already exists. Skipping creation..."
+}
+else {
+  $federatedApp = New-MgApplicationFederatedIdentityCredential -ApplicationId $clientId `
+    -Audiences api://AzureADTokenExchange -Issuer "https://token.actions.githubusercontent.com" -Name $gitFederationName -Subject $policy
+}
+Write-Output "devObjectId=$myApp.ObjectId" >> $env:GITHUB_ENV
